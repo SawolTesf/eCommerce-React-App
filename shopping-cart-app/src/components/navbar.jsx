8c620060d2954bb55f3c03b96854e5730f7e5c2f
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-07-28 23:18:05 -0500

    Add styling for navbar component using navbar.css and clear App.css styling

--- shopping-cart-app/src/components/navbar.jsx
+++ shopping-cart-app/src/components/navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react"
+import "./navbar.css";
 export const Navbar = () => {
-    return <div className="navbar">
-        <div className="links">
-            
+    return (
+        <div className="navbar">
+            <div className="links">
+                <Link to='/'> Shop </Link>
+                <Link to='/cart'> </Link>
+                <ShoppingCart size={32} ></ShoppingCart>
+            </div>
         </div>
-    </div>;
+    );
 };
