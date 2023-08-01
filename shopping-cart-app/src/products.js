38a385f1eba6fa0f79024d6f9c9eab1f04699ffb
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-01 18:15:24 -0500

    Create product objects for each product in products.js

--- shopping-cart-app/src/products.js
+++ shopping-cart-app/src/products.js
@@ -1,10 +1,20 @@
 // products.js
 let products = [];
+let id = 0;
 
 fetch("https://fakestoreapi.com/products", { mode: "cors" })
   .then((response) => response.json())
   .then((response) => {
-    response.forEach(product => console.log(product));
+    response.forEach((product) => {
+        let productObj = {
+            id: id,
+            title: product.title,
+            price: product.price,
+            description: product.description,
+        };
+        products.push(productObj);
+        id++;
+    });
   })
   .catch((error) => console.log(error));
 
