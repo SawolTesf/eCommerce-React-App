f4d062526179a31dd296c1751f4c6cedbd5452c7
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-01 20:23:03 -0500

    Add product details to the products array in products.js using forEach loop

--- shopping-cart-app/src/products.js
+++ shopping-cart-app/src/products.js
@@ -6,11 +6,14 @@ fetch("https://fakestoreapi.com/products", { mode: "cors" })
   .then((response) => response.json())
   .then((response) => {
     response.forEach((product) => {
+        console.log(product);
         let productObj = {
             id: id,
             title: product.title,
             price: product.price,
             description: product.description,
+            category: product.category,
+            image: product.image,
         };
         products.push(productObj);
         id++;
