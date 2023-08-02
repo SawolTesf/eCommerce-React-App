275914a1eaff227b65e295b26bf06dbbc1fcc8de
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-01 20:26:02 -0500

    Create Product component in products.jsx and use it to render products in shop.jsx

---
+++ shopping-cart-app/src/pages/shop/product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Product = (props) => {
+    const { id, title, price, description, category, image } = props.data;
+    return (
+        <div className='product'>
+            
+        </div>
+    )
+}
--- shopping-cart-app/src/pages/shop/shop.jsx
+++ shopping-cart-app/src/pages/shop/shop.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { products } from '../../products';
+import { Product } from './product';
 
 export const Shop = () => {
   return (
     <div className='shop'>
         <div className='shop-Title'>
             <h1> Welcome to Our Online Shop </h1>
+        </div>
+        <div className='products'>
+          {products.map((product) => (
+            <Product data={product}/>
+          ))}
         </div>
     </div>
   );
