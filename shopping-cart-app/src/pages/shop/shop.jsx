df35b6789e22e23223a7568dfefa47265f282993
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-04 20:13:40 -0500

    Render product details in products.js and implement fetching products in shop.js using useState and useEffect

--- shopping-cart-app/src/pages/shop/shop.jsx
+++ shopping-cart-app/src/pages/shop/shop.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import { products } from '../../products';
+import React, { useState, useEffect } from 'react'
+import { getProducts } from '../../products';
 import { Product } from './product';
 
 export const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getProducts()
+      .then((products) => {
+        setProducts(products);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='shop'>
         <div className='shop-Title'>
--- shopping-cart-app/src/products.js
+++ shopping-cart-app/src/products.js
@@ -1,24 +1,15 @@
-// products.js
-let products = [];
-let id = 0;
-
-fetch("https://fakestoreapi.com/products", { mode: "cors" })
-  .then((response) => response.json())
-  .then((response) => {
-    response.forEach((product) => {
-        console.log(product);
-        let productObj = {
-            id: id,
-            title: product.title,
-            price: product.price,
-            description: product.description,
-            category: product.category,
-            image: product.image,
-        };
-        products.push(productObj);
-        id++;
-    });
-  })
-  .catch((error) => console.log(error));
-
-export { products };
+export const getProducts = () => {
+    return fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => {
+        return response.map((product, index) => ({
+          id: index + 1,
+          title: product.title,
+          price: product.price,
+          description: product.description,
+          category: product.category,
+          image: product.image,
+        }));
+      });
+  };
+  
