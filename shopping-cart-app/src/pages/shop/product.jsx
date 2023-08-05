39a4e02563217c18592e7931ebc3070053247703
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-04 21:14:05 -0500

    Add test elements for each product in Product component

--- shopping-cart-app/src/pages/shop/product.jsx
+++ shopping-cart-app/src/pages/shop/product.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 export const Product = (props) => {
     const { id, title, price, description, category, image } = props.data;
+
     return (
-        <div className='product'>
-            
+        <div className='products'>
+            <div className='item'>
+                <p>{id}</p>
+                <p>{title}</p>
+                <p>{price}</p>
+                <p>{description}</p>
+                <p>{category}</p>
+                <img src={image} alt={title} />            
+            </div>
+
         </div>
     )
 }
