f5ceb708c4c0c774f4ba120accdd09382d5e925e
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-10 20:24:15 -0500

    Create 'Shop Context' using shop-context.jsx and enhance styling for product display and 'Add to Cart' buttons

---
+++ shopping-cart-app/context/shop-context.jsx
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react'
+
+export const ShopContext = createContext(null);
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState([]);
+  return (
+    <ShopContext.Provider>{props.children}</ShopContext.Provider>
+  )
+}
--- shopping-cart-app/src/pages/shop/product.jsx
+++ shopping-cart-app/src/pages/shop/product.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 export const Product = (props) => {
     const { id, title, price, description, category, image } = props.data;
+    console.log(id);
+    console.log(description);
     return (
-        <div className='product mt-4 flex flex-col items-center justify-around border border-gray-300 p-5'>
+        <div className='product mt-4 flex flex-col items-center justify-around border border-black-300 p-5'>
             <img src={image} alt={title} className='MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img w-full css-1ri5oru h-50 object-contain'/>
             <div className='description mt-5 flex-1'>                
-                <p>{title}</p>
-                <p className='text-sm font-medium text-gray-900'><b>${price}</b></p>
-                <p>{category}</p>
+                <p className='h-6 overflow-hidden mb-6'>{title}</p>
+                <p className='h-6 overflow-hidden mb-6'>{category}</p>
+                <p className='font-bold text-xl h-6 overflow-hidden mb-6'><b>${price}</b></p>
             </div>
-            <button className='addToCartbtn font-bold'> Add To Cart </button>
+            <button className='addToCartbtn transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none p-2'> Add To Cart </button>
         </div>
     )
-    
+
 }
