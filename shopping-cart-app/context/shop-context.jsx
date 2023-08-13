cf24df715aa9e3f1c1e26c779e92cc431f955d59
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-12 20:51:23 -0500

    Implement shopping cart functionality with addToCart, removeFromCart, and getDefaultCart functions in ShopContextProvider

--- shopping-cart-app/context/shop-context.jsx
+++ shopping-cart-app/context/shop-context.jsx
@@ -2,8 +2,27 @@ import React, { createContext } from 'react'
 
 export const ShopContext = createContext(null);
 
+// This function will set each product's quantity to 0
+
+const getDefaultCart = () => {
+  let cart = {};
+  for(let i = 0; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+}
+
 export const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState(getDefaultCart()); 
+
+    const addToCart = (id) => {
+      setCartItems((prev) => ({ ...prev, [id]: prev[id] + 1 }));
+    };
+
+    const removeFromCart = (id) => {
+      setCartItems((prev) => ({ ...prev, [id]: prev[id] - 1 }));
+    };
+      
   return (
     <ShopContext.Provider>{props.children}</ShopContext.Provider>
   )
