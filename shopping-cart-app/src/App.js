76e781eed66fc73c5cefc4c15c59237a7155a992
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-13 15:40:01 -0500

    Wrapped the App with ShopContextProvider for global state management

--- shopping-cart-app/src/App.js
+++ shopping-cart-app/src/App.js
@@ -3,24 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
 import { Cart } from "./pages/cart/cart";
+// Importing the ShopContextProvider component
+import { ShopContextProvider } from "./context/shop-context";
 
 // App component
 function App() {
 
   return (
     <div className="App">
-      {/* Setting up the router */}
-      <Router>
-        {/* Navbar component */}
-        <Navbar />
-        {/* Defining the routes */}
-        <Routes>
-          {/* Route for the Shop page */}
-          <Route path="/" element={<Shop/>} />
-          {/* Route for the Cart page */}
-          <Route path="/cart" element={<Cart/>} />
-        </Routes>
-      </Router>
+      {/* Wrapping the Router component with the ShopContextProvider */}
+      {/* This provides the ShopContext to all child components of the Router */}
+      <ShopContextProvider>
+        {/* Setting up the router */}
+        <Router>
+          {/* Navbar component */}
+          <Navbar />
+          {/* Defining the routes */}
+          <Routes>
+            {/* Route for the Shop page */}
+            <Route path="/" element={<Shop/>} />
+            {/* Route for the Cart page */}
+            <Route path="/cart" element={<Cart/>} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
