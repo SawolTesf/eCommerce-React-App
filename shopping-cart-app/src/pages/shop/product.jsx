067f42eab1f7266bc8a4120edcf1dcd31927f371
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-13 15:34:38 -0500

    Added functionality to add products to the shopping cart in Product.jsx using ShopContext

--- shopping-cart-app/src/pages/shop/product.jsx
+++ shopping-cart-app/src/pages/shop/product.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 // Exporting the Product component
 export const Product = (props) => {
     // Destructuring the data object from props
     const { id, title, price, description, category, image } = props.data;
+    const { addToCart } = React.useContext(ShopContext);
     // Logging the id and description to the console
-    console.log(id);
-    console.log(description);
+
+    //console.log(id);
+    //console.log(description);
+    
     // Returning the JSX for the Product component
     return (
         <div className='product mt-4 flex flex-col items-center justify-around border border-black-300 p-5'>
@@ -19,7 +23,7 @@ export const Product = (props) => {
                 <p className='font-bold text-xl h-6 overflow-hidden mb-6'><b>${price}</b></p>
             </div>
             {/* Add to cart button */}
-            <button className='addToCartbtn transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none p-2'> Add To Cart </button>
+            <button className='addToCartbtn transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none p-2' onClick={() => addToCart(id)}> Add To Cart </button>
         </div>
     )
 }
