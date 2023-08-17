e76ae42c39d008f78878b3526a23d9701a428ad7
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-16 20:22:25 -0500

    Implemented the Cart component in cart.jsx to display items added to the cart, utilizing context and mapping over products

--- shopping-cart-app/src/pages/cart/cart.jsx
+++ shopping-cart-app/src/pages/cart/cart.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import { getProducts } from '../../products';
+import { CartItem } from './cartItem';
+import { ShopContext } from '../../context/shop-context';
 
 export const Cart = () => {
+  const {cartItems} = useContext(ShopContext); 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div>cart</div>
+    <div className='Cart'>
+      <div className='text-center text-5xl font-bold'>
+        <h1>Cart Items</h1>
+      </div>
+      <div className='cartItems'>
+        {products.map((product) => {
+          if(cartItems[product.id] > 0){
+            console.log(product.id);
+            return <CartItem key={product.id} data={product} />
+          }
+        })}
+      </div>
+    </div>
   );
 }
