c9942e6b5c7d2d91cd378afeadc9d0f5dd0c396c
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-08-18 23:16:33 -0500

    Fixed layout issues in cart.jsx for proper background extension and spacing

--- shopping-cart-app/src/pages/cart/cart.jsx
+++ shopping-cart-app/src/pages/cart/cart.jsx
@@ -19,7 +19,7 @@ export const Cart = () => {
   let total = subtotal + shippingCost;
 
   return (
-    <div className='Cart h-screen bg-gray-100 pt-20'>
+    <div className='Cart bg-gray-100 pt-20 min-h-screen'>
       <div className='text-center text-5xl font-bold'>
         <h1>Cart Items</h1>
       </div>
@@ -36,16 +36,16 @@ export const Cart = () => {
         {subtotal > 0 && (
           <div className='mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3'>
             <div className='mb-2 flex justify-between'>
-              <p className='class="text-gray-700'>Subtotal</p>
+              <p className='class="text-gray-700 mr-4'>Subtotal</p>
               <p className='class="text-gray-700'>${subtotal.toFixed(2)}</p>
             </div>
             <div className='flex justify-between'>
-              <p className='text-gray-700'>Shipping</p>
+              <p className='text-gray-700 mr-4'>Shipping</p>
               <p className='text-gray-700'>${shippingCost.toFixed(2)}</p>
             </div>
             <hr className='my-4'></hr>
             <div className='flex justify-between'>
-              <p className='text-lg font-bold'>Total</p>
+              <p className='text-lg font-bold mr-4'>Total</p>
               <div className=''>
                 <p className='mb-1 text-lg font-bold'>${total.toFixed(2)} USD</p>
                 <p className='text-sm text-gray-700'>including VAT</p>
